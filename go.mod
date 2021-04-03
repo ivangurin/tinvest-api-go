ee71795e044c06fc1562f0b29c30888a5578b4d1
Ivan Gurin <ivan@gurin.su>
2021-04-03 15:41:42 +0300

    client was updated

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/ivangurin/tinvest-analyser-go v0.0.0-20210326124013-1016122f94ce
-	github.com/ivangurin/tinvest-client-go v0.0.0-20210323123106-d2ba38e8fe4a
+	github.com/ivangurin/tinvest-analyser-go v0.0.0-20210403112514-f05773b703b8
+	github.com/ivangurin/tinvest-client-go v0.0.0-20210403122857-ea7ba282bc95
 	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/ivangurin/tinvest-analyser-go"
-	"github.com/ivangurin/tinvest-client-go"
 	"log"
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/gorilla/mux"
+	tinvestanalyser "github.com/ivangurin/tinvest-analyser-go"
+	tinvestclient "github.com/ivangurin/tinvest-client-go"
 )
 
 func returnRoot(ioResponse http.ResponseWriter, ioRequest *http.Request) {
@@ -76,6 +77,79 @@ func returnPositions(ioResponse http.ResponseWriter, ioRequest *http.Request) {
 
 }
 
+func returnProfit(ioResponse http.ResponseWriter, ioRequest *http.Request) {
+
+	fmt.Print("\n", time.Now().Format(time.RFC3339), " ", ioRequest.URL.Path, " - ")
+
+	ioResponse.Header().Add("Access-Control-Allow-Origin", "*")
+	ioResponse.Header().Add("Access-Control-Allow-Methods", "*")
+	ioResponse.Header().Add("Access-Control-Allow-Headers", "*")
+
+	if ioRequest.Method == http.MethodOptions {
+		return
+	}
+
+	lvBearerToken := ioRequest.Header.Get("Authorization")
+
+	if lvBearerToken == "" {
+		ioResponse.WriteHeader(http.StatusUnauthorized)
+		fmt.Println(http.StatusText(http.StatusUnauthorized))
+		return
+	}
+
+	loParameters, loError := url.ParseQuery(ioRequest.URL.RawQuery)
+
+	if loError != nil {
+		ioResponse.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(ioResponse, "%v", loError)
+		fmt.Println(loError)
+		return
+	}
+
+	lvTicker := ""
+
+	ltTickers, lvExists := loParameters["ticker"]
+
+	if lvExists {
+		if len(ltTickers) > 0 {
+			lvTicker = ltTickers[0]
+		}
+	}
+
+	loAnalyzer := tinvestanalyser.Analyser{}
+
+	loAnalyzer.Init(lvBearerToken[7:])
+
+	ltProfit, loError := loAnalyzer.GetProfit(lvTicker, time.Now().AddDate(-10, 0, 0), time.Now())
+
+	if loError != nil {
+		ioResponse.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(ioResponse, "%v", loError)
+		fmt.Println(loError)
+		return
+	}
+
+	if len(ltProfit) == 0 {
+		ltProfit = make([]tinvestanalyser.Profit, 0)
+	}
+
+	lvBody, loError := json.Marshal(ltProfit)
+
+	if loError != nil {
+		ioResponse.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(ioResponse, "%v", loError)
+		fmt.Println(loError)
+		return
+	}
+
+	ioResponse.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	ioResponse.Write(lvBody)
+
+	fmt.Print("OK")
+
+}
+
 func returnOperations(ioResponse http.ResponseWriter, ioRequest *http.Request) {
 
 	fmt.Print("\n", time.Now().Format(time.RFC3339), " ", ioRequest.URL.Path, "/", ioRequest.URL.RawQuery, " - ")
@@ -101,7 +175,7 @@ func returnOperations(ioResponse http.ResponseWriter, ioRequest *http.Request) {
 
 	lvTicker := ltVars["ticker"]
 
-	if lvTicker == ""{
+	if lvTicker == "" {
 		loError := errors.New("ticker is missing")
 		ioResponse.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(ioResponse, "%v", loError)
@@ -153,87 +227,14 @@ func returnOperations(ioResponse http.ResponseWriter, ioRequest *http.Request) {
 
 }
 
-func returnProfit(ioResponse http.ResponseWriter, ioRequest *http.Request) {
-
-	fmt.Print("\n", time.Now().Format(time.RFC3339), " ", ioRequest.URL.Path, " - ")
-
-	ioResponse.Header().Add("Access-Control-Allow-Origin", "*")
-	ioResponse.Header().Add("Access-Control-Allow-Methods", "*")
-	ioResponse.Header().Add("Access-Control-Allow-Headers", "*")
-
-	if ioRequest.Method == http.MethodOptions {
-		return
-	}
-
-	lvBearerToken := ioRequest.Header.Get("Authorization")
-
-	if lvBearerToken == "" {
-		ioResponse.WriteHeader(http.StatusUnauthorized)
-		fmt.Println(http.StatusText(http.StatusUnauthorized))
-		return
-	}
-
-	loParameters, loError := url.ParseQuery(ioRequest.URL.RawQuery)
-
-	if loError != nil {
-		ioResponse.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(ioResponse, "%v", loError)
-		fmt.Println(loError)
-		return
-	}
-
-	lvTicker := ""
-
-	ltTickers, lvExists := loParameters["ticker"]
-
-	if lvExists{
-		if len(ltTickers) > 0 {
-			lvTicker = ltTickers[0]
-		}
-	}
-
-	loAnalyzer := tinvestanalyser.Analyser{}
-
-	loAnalyzer.Init(lvBearerToken[7:])
-
-	ltProfit, loError := loAnalyzer.GetProfit(lvTicker, time.Now().AddDate(-10, 0, 0), time.Now())
-
-	if loError != nil {
-		ioResponse.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(ioResponse, "%v", loError)
-		fmt.Println(loError)
-		return
-	}
-
-	if len(ltProfit) == 0 {
-		ltProfit = make([]tinvestanalyser.Profit, 0)
-	}
-
-	lvBody, loError := json.Marshal(ltProfit)
-
-	if loError != nil {
-		ioResponse.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(ioResponse, "%v", loError)
-		fmt.Println(loError)
-		return
-	}
-
-	ioResponse.Header().Add("Content-Type", "application/json; charset=utf-8")
-
-	ioResponse.Write(lvBody)
-
-	fmt.Print("OK")
-
-}
-
 func main() {
 
 	loRouter := mux.NewRouter()
 
 	loRouter.HandleFunc("/", returnRoot).Methods(http.MethodGet)
 	loRouter.HandleFunc("/positions", returnPositions).Methods(http.MethodGet, http.MethodOptions)
-	loRouter.HandleFunc("/operations/{ticker}", returnOperations).Methods(http.MethodGet, http.MethodOptions)
 	loRouter.HandleFunc("/profit", returnProfit).Methods(http.MethodGet, http.MethodOptions)
+	loRouter.HandleFunc("/operations/{ticker}", returnOperations).Methods(http.MethodGet, http.MethodOptions)
 
 	log.Fatal(http.ListenAndServe(":8081", loRouter))
 
